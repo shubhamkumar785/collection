e80e702e0ac74b7035991a7c00c6218ecbd64d40
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-02-05 14:12:36 +0530

    Update collection.java

--- Assignment/collection.java
+++ Assignment/collection.java
@@ -4,13 +4,17 @@
 // Ans: Collection framework is used to represent a group of object and java collection provide a classes and interface for us to be able to write code quickly and efficiently.public class collection {
 
 
-// Implement of Set
+
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class collection {
     public static void main(String[] args) {
+
+        // Implement of Set
         Set<String> s1 = new HashSet<>(); 
         s1.add("Shubham");
         s1.add("sourav");
@@ -21,29 +25,27 @@ public class collection {
         System.out.println(s1.contains("subhanshu"));
         s1.remove("golu");
         System.out.println(s1);
+
+
+        // Implementation of list
+
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        l1.add(1);
+        
+        for(int i=0; i<l1.size(); i++){
+            System.out.println(l1.get(i));
+        }
+        l1.remove(2);
+        System.out.println(l1);
         
     }
 }
 
 
-// Implementation of list
-
-// import java.util.ArrayList;
-// import java.util.List;
 
 
-// public class collection {
-//     public static void main(String[] args) {
-//         List<Integer> l1 = new ArrayList<>();
-//         l1.add(1);
-//         l1.add(2);
-//         l1.add(3);
-//         l1.add(1);
-        
-//         for(int i=0; i<l1.size(); i++){
-//             System.out.println(l1.get(i));
-//         }
-//         l1.remove(2);
-//         System.out.println(l1);
-//     }
-// }
+
+
